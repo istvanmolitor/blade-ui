42668c2fd9cf6a945ee48a3d94b3913eba80be73
Molitor István <istvanmolitor@gmail.com>
2026-07-20 21:50:24 +0200

    login

--- resources/views/components/email-field.blade.php
+++ resources/views/components/email-field.blade.php
@@ -1,7 +1,7 @@
-@props(['name', 'label', 'type' => 'text', 'value' => null])
+@props(['id', 'label', 'value' => null, 'required' => false, 'autofocus' => false])
 
 <div {{ $attributes->merge(['class' => 'mb-4']) }}>
-    <x-ui::label :for="$name" :value="$label" />
-    <x-ui::input type="email" :name="$name" :value="$value" />
-    <x-ui::field-error :name="$name" />
+    <x-blade-ui::label :for="$id" :value="$label" />
+    <x-blade-ui::input type="email" :name="$id" :id="$id" :value="$value" :required="$required" :autofocus="$autofocus" />
+    <x-blade-ui::field-error :name="$id" />
 </div>
